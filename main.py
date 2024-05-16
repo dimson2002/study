9c7a72b2d2f782a37bf97ca7a8f41c1e061ff53e
235379 <dimson2002@gmail.com>
2024-05-16 20:39:20 +0300

    initial fix

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ def get_html(url, params=None):
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items =soup.find_all('div', class_='d-subject__group')
-
+    items = soup.find_all('div', class_='d-content__block')
+    stud = []
+    for item in items:
+        stud.append({
+            'Факультет': item.find('div', class_='d-subject__title').get_text(strip=True),
+            'Направление': item.find('div', class_='d-subject__group-txt').get_text(strip=True)
+        })
+    print(stud)
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
